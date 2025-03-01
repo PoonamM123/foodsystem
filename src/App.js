23eb2188a9556572de8f613494b3acf5b421e919
poonam <poonammehandiya111@gmail.com>
2025-03-02 05:10:28 +0530

    Added routing and deployment setup

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import FoodEntryPage from "./pages/FoodEntryPage";
 import FoodListPage from "./pages/FoodListPage";
 
 const App = () => {
   const [foodEntries, setFoodEntries] = useState([]);
 
-  // Load saved data from localStorage
   useEffect(() => {
     const savedEntries = JSON.parse(localStorage.getItem("foodEntries")) || [];
     setFoodEntries(savedEntries);
   }, []);
 
-  // Save data to localStorage whenever entries change
+
   useEffect(() => {
     localStorage.setItem("foodEntries", JSON.stringify(foodEntries));
   }, [foodEntries]);
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<FoodEntryPage setFoodEntries={setFoodEntries} />} />
-        <Route path="/food-list" element={<FoodListPage foodEntries={foodEntries} />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<FoodEntryPage setFoodEntries={setFoodEntries} />} />
+      <Route path="/food-list" element={<FoodListPage foodEntries={foodEntries} />} />
+    </Routes>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // Yeh add karna zaroori hai
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/foodsystem"> {/* Yeh add karo */}
+    <BrowserRouter basename="/<your-repo-name>"> {/* अपना रेपो नाम डालें */}
       <App />
     </BrowserRouter>
   </React.StrictMode>
